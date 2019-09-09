3157f43de42da0d0a6496af74fc64d20e0b6f572
AShelganov <A.E.Shelganov@mgts.ru>
2019-09-09 10:46:52 +0300

    09.09.2019

--- BookStore/Controllers/FootballController.cs
+++ BookStore/Controllers/FootballController.cs
@@ -34,5 +34,53 @@ namespace BookStore.Controllers
             return View(team);
         }
 
+        [HttpGet]
+        public ActionResult Create()
+        {
+            // Формируем список команд для передачи в представление
+            SelectList teams = new SelectList(db.Teams, "Id", "Name");
+            ViewBag.Teams = teams;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Player player)
+        {
+            //Добавляем игрока в таблицу
+            db.Players.Add(player);
+            db.SaveChanges();
+            // перенаправляем на главную страницу
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return HttpNotFound();
+            }
+            // Находим в бд футболиста
+            Player player = db.Players.Find(id);
+            if (player != null)
+            {
+                // Создаем список команд для передачи в представление
+                SelectList teams = new SelectList(db.Teams, "Id", "Name", player.TeamId);
+                ViewBag.Teams = teams;
+                return View(player);
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Player player)
+        {
+            db.Entry(player).State = EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+
+
     }
 }
