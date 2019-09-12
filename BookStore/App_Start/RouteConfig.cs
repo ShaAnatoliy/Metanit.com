e2c60964b9a3980bbeec4fbc59a2542cd861a7fe
AShelganov <A.E.Shelganov@mgts.ru>
2019-09-12 17:44:36 +0300

    17:40

--- BookStore/App_Start/RouteConfig.cs
+++ BookStore/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace BookStore
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                defaults: new { controller = "Book", action = "Index", id = UrlParameter.Optional },
                 namespaces: new[] { "BookStore.Controllers" }
             );
         }
