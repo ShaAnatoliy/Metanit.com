fd1d15ffe01c1ae32babbdc3adaaeee480716853
AShelganov <A.E.Shelganov@mgts.ru>
2019-09-12 17:23:18 +0300

    17:20

---
+++ BookStore/Models/Book.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BookStore.Models
+{
+    public class Book
+    {
+        // ID книги
+        [HiddenInput(DisplayValue = false)]
+        public int Id { get; set; }
+
+        [Display(Name = "Название")]
+        public string Name { get; set; }
+
+        [Display(Name = "Автор")]
+        public string Author { get; set; }
+
+        [Display(Name = "Цена")]
+        public int Price { get; set; }
+    }
+}
