0d4dfcc25f4b22cd277ea7f18154996539bd9338
AShelganov <A.E.Shelganov@mgts.ru>
2019-09-13 17:23:59 +0300

    17:20

---
+++ Books/SQLiteDB/SQLiteDB.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Books
+{
+    public static class SQLiteDB
+    {
+        /// <summary> Путь к рабочему каталогу, относительно исполняемого файла </summary>
+        static string dirWork { get; set; }
+        /// <summary> Путь к файлу базы данных </summary>
+        static string dbPath { get; set; }
+        /// <summary> Имя таблицы базы данных </summary>
+        static string dbTableName { get; set; }
+
+
+    }
+}
