8c25cc0d9784e6d61c9132f3528f2a7b59ec89c1
AShelganov <A.E.Shelganov@mgts.ru>
2019-09-27 11:00:46 +0300

    +DepInject

---
+++ DepInject/Controllers/BookController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Books.Controllers
+{
+    using Models;
+
+    public class BookController : ApiController
+    {
+        IBookRepository _repository;
+
+        public BookController(IBookRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public IEnumerable<Book> Get()
+        {
+            return _repository.GetAll();
+        }
+
+        public IHttpActionResult Get(int id)
+        {
+            var book = _repository.GetByID(id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+            return Ok(book);
+        }
+
+        public IHttpActionResult Post(Book book)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            _repository.Add(book);
+            return CreatedAtRoute("DefaultApi", new { id = book.Id }, book);
+        }
+    }
+}
---
+++ DepInject/Models/Book.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace Books.Models
+{ // var conn1 = db.Database.Connection;
+    public class Book
+    {
+        [HiddenInput(DisplayValue = false)]
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Название")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Длина строки должна быть от 3 до 50 символов")]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name = "Автор")]
+        public string Author { get; set; }
+
+        // [Range(typeof(decimal), "0.00", "49.99")]
+        [Required(ErrorMessage = "Цена должна быть определена")]
+        [Display(Name = "Цена")]
+        public double Price { get; set; }
+
+        [Required]
+        [Display(Name = "Год")]
+        [Range(1700, 2022, ErrorMessage = "Недопустимый год")]
+        public int Year { get; set; }
+    }
+}
